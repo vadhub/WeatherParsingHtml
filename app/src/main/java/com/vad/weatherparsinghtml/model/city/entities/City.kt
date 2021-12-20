3b2a6be4303a1612429092df8ba3f82ad44684b0
Vadim <gabderahmanov99@gmail.com>
2021-12-20 12:14:44 +0300

    add entity city

---
+++ app/src/main/java/com/vad/weatherparsinghtml/model/city/entities/City.kt
@@ -0,0 +1,18 @@
+package com.vad.weatherparsinghtml.model.city.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "cities",
+    indices = [
+        Index("name", unique = true) //unique name city
+    ]
+)
+data class City(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    @ColumnInfo(collate = ColumnInfo.NOCASE) val name: String //Abg == abg
+) {
+}
