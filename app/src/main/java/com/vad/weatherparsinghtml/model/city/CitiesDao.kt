380983eaa81acd8a5e22489afbe267dc1ea40749
Vadim <gabderahmanov99@gmail.com>
2021-12-21 13:19:47 +0300

    add method for get all cities

--- app/src/main/java/com/vad/weatherparsinghtml/model/city/CitiesDao.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/model/city/CitiesDao.kt
@@ -1,13 +1,18 @@
 package com.vad.weatherparsinghtml.model.city
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.vad.weatherparsinghtml.model.city.entities.City
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CitiesDao {
+
+    @Query("SELECT * FROM cities ORDER BY id ASC")
+    suspend fun getAll(): LiveData<List<City>>
 
     @Query("SELECT * from cities WHERE name = :name")
     suspend fun findByName(name: String): City?
@@ -15,7 +20,6 @@ interface CitiesDao {
     @Update
     suspend fun updateCityName(city: City)
 
-    @Insert
     suspend fun addCity(city: City)
 
     @Query("DELETE FROM cities WHERE id =:id")
