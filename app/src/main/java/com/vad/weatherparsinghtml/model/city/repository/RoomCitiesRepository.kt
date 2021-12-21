224ab44dde1f0eb682bacbd6e2c7408239ccc89b
Vadim <gabderahmanov99@gmail.com>
2021-12-21 13:40:50 +0300

    delete private signature & get all cities

--- app/src/main/java/com/vad/weatherparsinghtml/model/city/repository/RoomCitiesRepository.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/model/city/repository/RoomCitiesRepository.kt
@@ -1,18 +1,21 @@
 package com.vad.weatherparsinghtml.model.city.repository
 
 import android.database.sqlite.SQLiteConstraintException
+import androidx.lifecycle.LiveData
 import com.vad.weatherparsinghtml.model.city.CitiesDao
 import com.vad.weatherparsinghtml.model.city.entities.City
 
 class RoomCitiesRepository(
     private val dao: CitiesDao
 ) {
+
+    val getAllCities: LiveData<List<City>> = dao.getAllCities()
     private suspend fun findCitiesByName(name: String): Long? {
         val city = dao.findByName(name)
         return city?.idCity
     }
 
-    private suspend fun addCity(city: City) {
+    suspend fun addCity(city: City) {
         try {
             dao.addCity(city)
         } catch (e: SQLiteConstraintException) {
@@ -20,11 +23,11 @@ class RoomCitiesRepository(
         }
     }
 
-    private suspend fun updateCity(city: City) {
+    suspend fun updateCity(city: City) {
         dao.updateCityName(city)
     }
 
-    private suspend fun deleteCityById(id: Long) {
+    suspend fun deleteCityById(id: Long) {
         dao.deleteCity(id)
     }
 }
