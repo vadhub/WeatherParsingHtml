ab5f86138bef2229053209265ecea3f7def4b353
Vadim <gabderahmanov99@gmail.com>
2021-12-23 12:31:15 +0300

    add adapter

---
+++ app/src/main/java/com/vad/weatherparsinghtml/screens/viewweather/AdapterWeather.kt
@@ -0,0 +1,39 @@
+package com.vad.weatherparsinghtml.screens.viewweather
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vad.weatherparsinghtml.R
+import com.vad.weatherparsinghtml.model.weather.entities.Weather
+
+class AdapterWeather(weathers: List<Weather>) : RecyclerView.Adapter<AdapterWeather.MyViewHolder>() {
+
+    private var weathers: List<Weather> = weathers
+        set(weathers) {
+            field = weathers
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val viewHolder = LayoutInflater.from(parent.context).inflate(R.layout.item_city_weather, parent, false)
+        return MyViewHolder(viewHolder)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.cityName.text = weathers[position].cityId.toString()
+        holder.degree.text = weathers[position].weather.toString()
+        holder.date.text = weathers[position].date.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return weathers.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cityName = itemView.findViewById<TextView>(R.id.textViewCity)
+        val degree = itemView.findViewById<TextView>(R.id.textViewDegree)
+        val date = itemView.findViewById<TextView>(R.id.textViewDate)
+    }
+}
