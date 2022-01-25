2279f5d91158058de9aad86c6d7fd99d58316b4b
Vadim <gabderahmanov99@gmail.com>
2022-01-25 14:04:04 +0300

    listener on setonclick

--- app/src/main/java/com/vad/weatherparsinghtml/screens/addcity/AddCityDialogFragment.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/screens/addcity/AddCityDialogFragment.kt
@@ -1,14 +1,23 @@
 package com.vad.weatherparsinghtml.screens.addcity
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.vad.weatherparsinghtml.R
 import com.vad.weatherparsinghtml.databinding.DialogFragmentAddCityBinding
 
-class AddCityDialogFragment : Fragment() {
+class AddCityDialogFragment : DialogFragment() {
+
+    private lateinit var datable: Datable
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        datable = activity as Datable
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +25,10 @@ class AddCityDialogFragment : Fragment() {
     ): View? {
         val rootView: DialogFragmentAddCityBinding = DialogFragmentAddCityBinding.inflate(layoutInflater)
 
+        rootView.addBtn.setOnClickListener {
+            datable.setNameCity(rootView.cityName.text.toString())
+            dialog?.dismiss()
+        }
         return rootView.root
     }
 }
