db356dfe00d2a8843935b981fc7455e4846076d8
Vadim <gabderahmanov99@gmail.com>
2022-01-28 14:15:49 +0300

    add logic insert and read db fields

--- app/src/main/java/com/vad/weatherparsinghtml/screens/cities/CitiesFragment.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/screens/cities/CitiesFragment.kt
@@ -5,19 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import com.vad.weatherparsinghtml.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vad.weatherparsinghtml.databinding.FragmentCitiesBinding
+import com.vad.weatherparsinghtml.model.city.entities.City
+import com.vad.weatherparsinghtml.screens.addcity.AddCityDialogFragment
+import com.vad.weatherparsinghtml.screens.addcity.Datable
+import com.vad.weatherparsinghtml.viewmodel.ViewModelApp
 
-class CitiesFragment : Fragment() {
+class CitiesFragment : Fragment(), Datable {
+
+    private lateinit var viewModel: ViewModelApp
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val binding = FragmentCitiesBinding.inflate(inflater, container, false)
 
+        viewModel = ViewModelProvider(this).get(ViewModelApp::class.java)
+
+        val myRecyclerView = binding.citiesListRecycler
+        val adapter = CityAdapter()
+        myRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        myRecyclerView.adapter = adapter
+
+        viewModel.readAllData.observe(viewLifecycleOwner, Observer { cities ->
+            adapter.setCities(cities)
+        })
+
+        binding.floatingActionButton.setOnClickListener{
+            showDialog()
+        }
+
         return binding.root
+    }
+
+    override fun setNameCity(name: String) {
+       viewModel.addCity(City(0, name))
+    }
+
+    fun showDialog() {
+        val dialog = AddCityDialogFragment()
+        activity?.supportFragmentManager?.let { dialog.show(it, "Add city") }
     }
 }
