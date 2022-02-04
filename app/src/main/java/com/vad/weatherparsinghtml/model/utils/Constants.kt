81e469ef36530cbf03a1dec14ea81305b2912c15
Vadim <gabderahmanov99@gmail.com>
2022-02-04 14:20:20 +0300

    add Constants

---
+++ app/src/main/java/com/vad/weatherparsinghtml/model/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.vad.weatherparsinghtml.model.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "http://api.weatherapi.com/"
+        const val API_KEY = "0085f65f2c5b4f09b7c103212220402"
+    }
+}
