02b36d9f9605ccb39d11e81ebf66af96c46ddaca
Vadim <gabderahmanov99@gmail.com>
2022-02-04 14:16:36 +0300

    add retrofit service

---
+++ app/src/main/java/com/vad/weatherparsinghtml/model/api/ApiRetrofitService.kt
@@ -0,0 +1,11 @@
+package com.vad.weatherparsinghtml.model.api
+
+import androidx.lifecycle.LiveData
+import com.vad.weatherparsinghtml.model.pojo.Main
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiRetrofitService {
+    @GET("v1/current.json")
+    suspend fun getWeather(@Query("key") key: String, @Query("q") q: String, @Query("aqi") aqi: String): LiveData<Main>
+}
