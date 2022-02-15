a1e2237f121347c09f0bc3b163572ca6b89dcb5b
Vadim <gabderahmanov99@gmail.com>
2022-02-15 19:13:50 +0300

    some change

--- app/src/main/java/com/vad/weatherparsinghtml/screens/main/MainActivity.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/screens/main/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
