5022d62c33dc90b25f099972c1642d4fe96f4860
Vadim <gabderahmanov99@gmail.com>
2022-02-23 17:43:01 +0300

    request with name city

--- app/src/main/java/com/vad/weatherparsinghtml/screens/viewweather/WeatherFragment.kt
+++ app/src/main/java/com/vad/weatherparsinghtml/screens/viewweather/WeatherFragment.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.RecyclerView
 import com.vad.weatherparsinghtml.databinding.FragmentWeatherBinding
 import com.vad.weatherparsinghtml.model.api.repository.RepositoryApi
+import com.vad.weatherparsinghtml.model.room.city.entities.City
 import com.vad.weatherparsinghtml.model.room.weather.entities.Weather
 import com.vad.weatherparsinghtml.viewmodel.ViewModelAppFactory
 import com.vad.weatherparsinghtml.viewmodel.ViewModelCity
 import com.vad.weatherparsinghtml.viewmodel.ViewModelWeatherFactory
 import com.vad.weatherparsinghtml.viewmodel.ViewModelWeathers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class WeatherFragment : Fragment() {
 
@@ -29,21 +33,42 @@ class WeatherFragment : Fragment() {
         val recyclerView: RecyclerView = rootView.WeathersRecycler
         val adapterWeather = AdapterWeather()
 
-        val nameCity = ""
-
         val repositoryApi = RepositoryApi()
 
-        val viewModelWeatherFactory = ViewModelWeatherFactory(requireActivity().application, repositoryApi)
-        viewModelWeather = ViewModelProvider(requireActivity(), viewModelWeatherFactory).get(ViewModelWeathers::class.java)
+        val viewModelWeatherFactory =
+            ViewModelWeatherFactory(requireActivity().application, repositoryApi)
+        viewModelWeather = ViewModelProvider(
+            requireActivity(),
+            viewModelWeatherFactory
+        ).get(ViewModelWeathers::class.java)
 
         val viewModelAppFactory = ViewModelAppFactory(requireActivity().application)
-        viewModelCity = ViewModelProvider(requireActivity(), viewModelAppFactory).get(ViewModelCity::class.java)
+        viewModelCity =
+            ViewModelProvider(requireActivity(), viewModelAppFactory).get(ViewModelCity::class.java)
+
+        viewModelCity?.readAllData?.observe(viewLifecycleOwner) { cities ->
+            cities.forEach { city ->
+                viewModelCity?.viewModelScope?.launch {
+                    viewModelWeather?.getWeather(
+                        city.nameCity
+                    )
+                }
+            }
+        }
+
 
         viewModelWeather?.myResponse?.observe(viewLifecycleOwner) { response ->
 
             if (response.isSuccessful) {
 
-                viewModelWeather!!.addWeather(Weather(0, 0, response.body()?.current?.tempC as Float, System.currentTimeMillis()))
+                viewModelWeather!!.addWeather(
+                    Weather(
+                        0,
+                        0,
+                        response.body()?.current?.tempC as Float,
+                        System.currentTimeMillis()
+                    )
+                )
             } else {
                 response.errorBody()
             }
@@ -51,4 +76,5 @@ class WeatherFragment : Fragment() {
 
         return rootView.root
     }
+
 }
